c594207400aaf880da2125a9e63625bba1d8c3a7
Chad <worktannoury@gmail.com>
2023-08-02 11:37:46 +0300

    changetarget backend location

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cool-routine-app-backend.vercel.app/',
         changeOrigin: true,
         secure: false,
       }
