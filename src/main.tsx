6da461ff41488da0b2c815376cdb67b25beb6562
Chad <worktannoury@gmail.com>
2023-08-02 22:05:31 +0300

    disable reactdevtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.tsx";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.Node_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
